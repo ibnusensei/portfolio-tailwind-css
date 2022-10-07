d649b1d4ac19a9b031b2194d7833aa4e39bcbeb6
ibnusensei <ibnu.setia23@gmail.com>
2022-10-07 12:04:12 +0800

    Update Portfolio Tailwind CSS

---
+++ dist/js/script.js
@@ -0,0 +1,21 @@
+// Navbar Fixed
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// Hamburger
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.querySelector('#nav-menu');
+console.log(hamburger);
+hamburger?.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('hidden');
+
+}); 
